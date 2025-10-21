c88f25715e2fd5ff495e0f45c2bf6b830bc6a15d
abdoboudad <electronicabdo5@gmail.com>
2025-10-21 16:08:20 +0100

    remove components option and add av-loop

--- avera-js.js
+++ avera-js.js
@@ -21,14 +21,15 @@ class Averajs{
             })
         }
     }
-    render() {
+    async render() {
         let html = this.template;
         for(const key in this.data){
             html = html.replaceAll(`{{${key}}}`,this.data[key])
         }
         this.el.innerHTML = html
         this.collectRefs()
-        this.loadComponents()
+        this.Avloop()
+        // await this.loadComponents()
     }
 
     collectRefs(){
@@ -40,23 +41,49 @@ class Averajs{
         });
     }
 
-    async loadComponents(){
-        const customTags = this.el.querySelectorAll('*');
-        for(const el of customTags){
-            const tagName = el.tagName.toLowerCase();
-            if(!["div","span","p","h1","h2","h3","h4","h5","h6","a","ul","li","button","section","article"].includes(tagName)){
-                const fileName = `${tagName}.html`;
-                try{
-                    const response = await fetch(fileName);
-                    const html = await response.text();
-                    el.innerHTML = html;
-                    this.collectRefs()
-                }catch(err){
-                    console.err(`Failed to load component ${tagName}:`, err);
-                    
-                }
+    Avloop(){
+        const loops = this.el.querySelectorAll("[av-loop]");
+        loops.forEach((loopEl)=>{
+            const loopExp = loopEl.getAttribute("av-loop").trim();
+            const [itemName,arrayName] = loopExp.split(" in ").map(s=>s.trim())
+            const array = this.data[arrayName]
+
+            if(Array.isArray(array)){
+                const parent = loopEl.parentNode;
+                array.forEach((val)=>{
+                    const clone = loopEl.cloneNode(true);
+                    clone.innerHTML = clone.innerHTML.replaceAll(`{{${itemName}}}`,val)
+                    parent.insertBefore(clone,loopEl)
+                })
             }
+            loopEl.remove()
+        })
+
+        let html = this.el.innerHTML;
+        for(const key in this.data){
+            html = html.replaceAll(`{{${key}}}`,this.data[key])
         }
+        this.el.innerHTML = html
     }
+    // async loadComponents(){
+    //     const customTags = this.el.querySelectorAll('*');
+    //     for(const el of customTags){
+    //         const tagName = el.tagName.toLowerCase();
+    //         if(!["div","span","p","h1","h2","h3","h4","h5","h6","a","ul","li","button","section","article"].includes(tagName)){
+    //             const fileName = `${tagName}.html`;
+    //             try{
+    //                 const response = await fetch(fileName);
+    //                 const html = await response.text();
+    //                 el.innerHTML = html;
+    //                 console.log(html);
+                    
+    //                 this.collectRefs()
+    //             }catch(err){
+    //                 console.err(`Failed to load component ${tagName}:`, err);
+                    
+    //             }
+    //         }
+    //     }
+    // }
 }
 
